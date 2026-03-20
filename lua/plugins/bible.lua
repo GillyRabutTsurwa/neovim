44faa834876a5506ac0ef90220b8a58889b74c24
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2026-03-19 23:57:29 -0400

    new bible plugin + fixed linting errors in php files

--- lua/plugins/bible.lua
+++ lua/plugins/bible.lua
@@ -1,41 +1,10 @@
 return {
-  "huynle/bible.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  keys = {
-    {
-      "<leader>bj",
-      "<Cmd>BibleLookupSelection {view = 'below'}<CR>",
-      mode = { "v" },
-    },
-    {
-      "<leader>bb",
-      "<Cmd>BibleLookupSelection<CR>",
-      mode = { "v" },
-    },
-    {
-      -- look up King James Version, if not defined, used default
-      "<leader>bK",
-      "<Cmd>BibleLookup {version = 'KJV'}<CR>",
-      mode = { "n" },
-    },
-    {
-      "<leader>bb",
-      "<Cmd>BibleLookup<CR>",
-      mode = { "n" },
-    },
-  },
-  opts = {
-    lookup_defaults = {
-      -- defaults, for more configuration look at lua/bible/config.lua
-      version = "NABRE", -- any version that is available on  BibleGateway
-      query = "Genesis 1:1", -- query can be split be commas, e.g. 'Gen 1:1, Jn 1:1'
-      view = "split",  -- 'split', 'below', 'right'
-      numbering = true,
-      footnotes = true,
-    }
-  },
+  "johnpgr/bible-reader.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    require("bible-reader").setup({
+      default_translation = "fr_apee",
+      language = "fr",
+    })
+  end,
 }
